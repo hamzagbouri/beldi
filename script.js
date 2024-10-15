29b014cb8ec84fa3e11c42bdfde1d45a7b15ee62
hamzagbouri <hamzagbouri2004@gmail.com>
2024-10-15 19:44:28 +0100

    est

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function() {
+	var navbar = document.querySelector(".navbar");
+	var header = document.querySelector("header");
+	var headerHeight = header.offsetHeight;
+
+	if (window.pageYOffset > headerHeight) {
+		navbar.classList.add("scrolled");
+	} else {
+		navbar.classList.remove("scrolled");
+	}
 });
